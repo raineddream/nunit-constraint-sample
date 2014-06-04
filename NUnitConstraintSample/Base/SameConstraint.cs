6849f9b4d774acbe2b12a827202f3c7a93ef80a2
raineddream <raineddream@gmail.com>
2014-06-04 11:49:25 +0800

    [ymeng] Sample for same constraint

---
+++ NUnitConstraintSample/Base/SameConstraint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Rain.NUnitConstraint.Base
+{
+    [TestFixture]
+    public class SameConstraint
+    {
+        [Test]
+        public void should_be_same()
+        {
+            Exception ex1 = new Exception();
+            Exception ex2 = ex1;
+
+            // CONSTRAINT
+            Assert.That(ex2, Is.SameAs(ex1));
+
+            // BASIC
+            Assert.AreSame(ex1, ex2);
+        }
+        [Test]
+        public void should_NOT_be_same()
+        {
+            Exception ex1 = new Exception();
+            Exception ex2 = new Exception();
+
+            // CONSTRAINT
+            Assert.That(ex2, Is.Not.SameAs(ex1));
+
+            // BASIC
+            Assert.AreNotSame(ex1, ex2);
+        }
+    }
+}
