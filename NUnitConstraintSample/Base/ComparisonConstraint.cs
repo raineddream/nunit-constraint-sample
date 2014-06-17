952c8e4ae93ab3506251e7b223a9fbeec9be9372
raineddream <raineddream@gmail.com>
2014-06-17 13:27:56 +0800

    [ymeng] Comparison constraint sample

---
+++ NUnitConstraintSample/Base/ComparisonConstraint.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace Rain.NUnitConstraint.Base
+{
+    [TestFixture]
+    public class ComparisonConstraint
+    {
+        [Test]
+        public void should_be_greater_than()
+        {
+            // CONSTRAINT
+            Assert.That(7, Is.GreaterThan(3));
+
+            // BASIC
+            Assert.Greater(7, 3);
+        }
+
+        [Test]
+        public void should_be_greater_than_or_equal_to()
+        {
+            // CONSTRAINT
+            Assert.That(7, Is.GreaterThanOrEqualTo(3));
+            Assert.That(7, Is.AtLeast(3));
+
+            // BASIC
+            Assert.GreaterOrEqual(7, 3);
+        }
+
+        [Test]
+        public void should_be_less_than()
+        {
+            // CONSTRAINT
+            Assert.That(3, Is.LessThan(7));
+
+            // BASIC
+            Assert.Less(3, 7);
+        }
+
+        [Test]
+        public void should_be_less_than_or_equal_to()
+        {
+            // CONSTRAINT
+            Assert.That(3, Is.LessThanOrEqualTo(7));
+            Assert.That(3, Is.AtMost(7));
+
+            // BASIC
+            Assert.LessOrEqual(3, 7);
+        }
+    }
+}
